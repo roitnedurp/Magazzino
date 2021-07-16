c8ecff97c94922a6e2d51683bd0da587709705d3
roitnedurp <roitnedurp@gmail.com>
2021-07-16 13:28:10 +0200

    fixes

--- lorenzoProject/lorenzoProject/settings.py
+++ lorenzoProject/lorenzoProject/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -75,17 +76,23 @@ WSGI_APPLICATION = 'lorenzoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {  
-	'default': {  
-		'ENGINE': 'django.db.backends.mysql',  
-		'NAME': 'lorenzo',  
-		'USER':'flavio',  
-		'PASSWORD':'27182818',  
-		'HOST':'10.12.0.12',                    # REMOTO
-		'PORT':'3306'  
-	}  
-}  
+#DATABASES = {  
+#	'default': {  
+#		'ENGINE': 'django.db.backends.mysql',  
+#		'NAME': 'lorenzo',  
+#		'USER':'flavio',  
+#		'PASSWORD':'27182818',  
+#		'HOST':'10.12.0.12',                    # REMOTO
+#		'PORT':'3306'  
+#	}  
+#}  
 
+DATABASES = { 
+        'default': { 
+                'ENGINE': 'django.db.backends.sqlite3', 
+                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), 
+                } 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
